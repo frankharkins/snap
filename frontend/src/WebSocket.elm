573595e536a0a36f99b3d22dc2ce1871a2070852
Frank Harkins <frankharkins@hotmail.co.uk>
2025-10-24 12:57:28 +0100

    fix URL again
    need a better solution for this...

--- frontend/src/WebSocket.elm
+++ frontend/src/WebSocket.elm
@@ -12,7 +12,7 @@ type Event =
   | MessageReceived String
 
 baseUrl : String
-baseUrl = "wss://snap-iegf.onrender.com"
+baseUrl = "wss://snap-image.onrender.com"
 
 joinGameUrl : String -> String
 joinGameUrl id = baseUrl ++ "/join/" ++ id
